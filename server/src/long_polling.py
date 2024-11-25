a78315894660a5551af8f9b8452ed5a8920ccd54
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-11-25 17:28:54 +0200

    do not clear messages and identify clients by address

--- server/src/long_polling.py
+++ server/src/long_polling.py
@@ -1,9 +1,11 @@
 from asyncio import sleep
+from collections import defaultdict
 from json import JSONDecodeError
 from time import time
 from typing import ClassVar
 
 from starlette import status
+from starlette.datastructures import Address
 from starlette.endpoints import HTTPEndpoint
 from starlette.requests import Request
 from starlette.responses import JSONResponse
@@ -11,17 +13,21 @@ from starlette.responses import JSONResponse
 
 class LongPollingMessages(HTTPEndpoint):
     TIMEOUT: float = 30.0
-    DELAY: float = 5.0
+    DELAY: float = 1.0
 
-    messages_queue: ClassVar[list[str]] = []
+    # Mapping of clients to messages offset.
+    clients: ClassVar[defaultdict[Address, int]] = defaultdict(lambda: 0)
+    messages: ClassVar[list[str]] = []
 
     async def get(self, request: Request) -> JSONResponse:
         start_time = time()
 
         while time() - start_time < self.TIMEOUT:
-            if self.messages_queue:
-                messages_to_send = self.messages_queue.copy()
-                self.messages_queue.clear()
+            messages_offset = self.clients[request.client]
+            messages_to_send = self.messages[messages_offset:]
+
+            if messages_to_send:
+                self.clients[request.client] = len(self.messages)
                 return JSONResponse({"messages": messages_to_send})
 
             # Non-blocking sleep, to continue processing new requests.
@@ -41,7 +47,7 @@ class LongPollingMessages(HTTPEndpoint):
             resp_code = status.HTTP_400_BAD_REQUEST
         else:
             if message:
-                self.messages_queue.append(message)
+                self.messages.append(message)
                 resp_message = "Message accepted"
                 resp_code = status.HTTP_201_CREATED
             else:
